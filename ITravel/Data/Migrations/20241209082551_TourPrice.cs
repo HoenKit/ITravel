2f63df9eb7e2e89f14bb242a52eaa45e67f7665d
HoenKit <hoangkaito03@gmail.com>
2024-12-09 15:44:57 +0700

    Add Price for Tour

---
+++ ITravel/Data/Migrations/20241209082551_TourPrice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITravel.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TourPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Tours",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Tours");
+        }
+    }
+}
