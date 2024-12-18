52a5f6f62ffb34ef7dfcf408e1c993d5f40c12e4
HoenKit <hoangkaito03@gmail.com>
2024-12-19 01:03:43 +0700

    Fix: GetReviewsByTourDateId Add: Update, Delete Review in Detail Page

--- ITravel/Repository/Implements/ReviewRepository.cs
+++ ITravel/Repository/Implements/ReviewRepository.cs
@@ -19,14 +19,65 @@ namespace ITravel.Repository.Implements
             _context.SaveChanges();
         }
 
-        public ICollection<Review> GetReviewsByTourDateId(Guid tourDateId) =>
+        public void DeleteReview(Guid id)
+        {
+            var review = GetReviewById(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+                _context.SaveChanges();
+            }
+        }
+
+        public Review GetReviewById(Guid id) =>
         _context.Reviews
-        .Include(r => r.Booking)
-            .ThenInclude(b => b.TourDate)
-        .Include(u => u.Booking)
-            .ThenInclude(ub => ub.User)
-        .Where(r => r.Booking.TourDate.Id == tourDateId)
-        .ToList();
-        
+        .Where(r => r.Id == id)
+        .FirstOrDefault();
+
+
+        public ICollection<Review> GetReviewsByTourDateId(Guid tourDateId)
+        {
+            var tourDate = _context.ToursDate
+                .Where(td => td.Id == tourDateId)
+                .FirstOrDefault();
+
+            if (tourDate != null)
+            {
+                // Get TourId
+                var tourId = tourDate.Tour.Id;
+
+                // Get All TourDate From TourId
+                var tourDates = _context.ToursDate
+                    .Where(td => td.Tour.Id == tourId) 
+                    .ToList();
+
+                // Get All Booking from TourDates
+                var bookings = _context.Bookings
+                    .Where(b => tourDates.Select(td => td.Id).Contains(b.TourDate.Id)) 
+                    .ToList();
+
+                // Get All Review from Booking
+                var reviews = _context.Reviews
+                    .Include(r => r.Booking)
+                        .ThenInclude(b => b.TourDate)
+                    .Include(r => r.Booking)
+                        .ThenInclude(b => b.User)
+                    .Where(r => bookings.Select(b => b.Id).Contains(r.Booking.Id)) 
+                    .ToList();
+                return reviews;
+            }
+            return null;
+        }
+
+
+        public void UpdateReview(Review review)
+        {
+            var updateReview = GetReviewById(review.Id);
+            if (updateReview != null)
+            {
+                _context.Reviews.Update(updateReview);
+                _context.SaveChanges();
+            }
+        }
     }
 }
